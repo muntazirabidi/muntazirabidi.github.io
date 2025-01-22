16a55ce3a39f6fc1d9adb4e755f351ff11f1d58d
Muntazir Abidi <smuntazirabidi@gmail.com>
2025-01-22 20:24:44 +0000

    Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/muntazirabidi.github.io/", // Replace with your repository name
+  base: "/", // For username.github.io repositories, use root path
 });
